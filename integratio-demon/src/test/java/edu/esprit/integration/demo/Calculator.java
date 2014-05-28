d211c39218a2b830de8c3c5151c924ed9c80f2a8
user <user@user-PC>
2014-05-28 15:01:48 +0100

    first commit
    Signed-off-by: user <user@user-PC>

---
+++ integratio-demon/src/test/java/edu/esprit/integration/demo/Calculator.java
@@ -0,0 +1,14 @@
+package edu.esprit.integration.demo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Calculator {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
